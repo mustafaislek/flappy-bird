94fa85b8f745bc3986ef617f54bb2482c5b7a983
mustafaislek <islekmustafaa@gmail.com>
2022-04-21 16:05:19 +0300

    First commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+  
+    void Start()
+    {
+        Destroy(gameObject, 7);
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
